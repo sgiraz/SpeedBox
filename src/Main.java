3c970ca5b4f4fa93d11d8b272bb1c7c50ac164ef
SimGira <s.girardi@gmail.com>
2016-03-25 17:56:52 +0100

    Prova del commit con eclipse

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.awt.EventQueue;
 public class Main {
 	
 	public static void main(String[] args) {
+		//prova
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
