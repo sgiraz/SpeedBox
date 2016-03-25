b8cfb063cb4d51f57cf42201ce90f677889c815c
SimGira <s.girardi@gmail.com>
2016-03-25 16:09:37 +0100

    testScript

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.awt.EventQueue;
 public class Main {
 	
 	public static void main(String[] args) {
+		System.out.println("Test");
 		
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
