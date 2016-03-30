bac10307523ce8bad6e741dd63de2e7f6bc2b698
SimGira <s.girardi@gmail.com>
2016-03-30 10:14:07 +0200

    30 mar

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ import java.awt.EventQueue;
 public class Main {
 	
 	public static void main(String[] args) {
-		//prova
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
