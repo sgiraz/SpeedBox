22e5463d8f135950540ef9aaa0ee8e75459392e7
SimoGira <s.girardi92@gmail.com>
2016-04-08 22:55:55 +0200

    LanNetwork complete..

--- src/wifi/LanNetwork.java
+++ src/wifi/LanNetwork.java
@@ -2,6 +2,10 @@ package wifi;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
@@ -11,14 +15,16 @@ import javax.swing.JSeparator;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 
-public class LanNetwork extends JDialog {
+public class LanNetwork extends JDialog implements ClosableWindow {
 
 	private static final long serialVersionUID = 8237689034968088432L;
 	private final JPanel contentPanel = new JPanel();
+	private Server server;
+	private JButton cancelButton;
 
 	public LanNetwork() {
+		server = new Server(this);
 		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-		setVisible(true);
 		setBounds(100, 100, 325, 147);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -48,11 +54,62 @@ public class LanNetwork extends JDialog {
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
 			{
-				JButton cancelButton = new JButton("Cancel");
+				cancelButton = new JButton("Cancel");
+				cancelButton.addActionListener(new ActionListener() 
+				{
+					public void actionPerformed(ActionEvent e) 
+					{
+						MainMenu.instance.setVisible(true);
+
+						System.out.println("Cancel clicked");
+						if(server != null)
+						{
+							System.out.println("Destroying server");
+							server.destroy();
+						}
+						destroy();
+					}
+				});
+				cancelButton.addActionListener(new ActionListener() 
+				{
+					public void actionPerformed(ActionEvent e) 
+					{
+						MainMenu.instance.setVisible(true);
+
+						System.out.println("Cancel clicked");
+						if(server != null)
+						{
+							System.out.println("Destroying server");
+							server.destroy();
+						}
+						destroy();
+					}
+				});
 				cancelButton.setActionCommand("Cancel");
 				buttonPane.add(cancelButton);
+
+				setLocationRelativeTo(null);
+				addWindowListener(new WindowAdapter()
+		        {
+		            public void windowClosing(WindowEvent e)
+		            {
+		                System.out.println("NetworkConfig.java: Closed");
+		                cancelButton.doClick();
+		            }
+		        });
 			}
 		}
+		setLocationRelativeTo(null);
+		setVisible(true);
+	}
+	@Override
+	public void destroy() {
+		dispose();
+	}
+
+	@Override
+	public void setVisibility(boolean visible) {
+		setVisible(visible);
 	}
 
 }
