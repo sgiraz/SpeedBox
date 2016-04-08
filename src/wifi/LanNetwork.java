c96cef2b8ac259df548f8c41db9e458ef131dd48
SimoGira <s.girardi92@gmail.com>
2016-04-08 22:32:42 +0200

    fixed MainMenu for Windows

--- src/wifi/LanNetwork.java
+++ src/wifi/LanNetwork.java
@@ -7,6 +7,10 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.JSeparator;
+import java.awt.Dimension;
 
 public class LanNetwork extends JDialog {
 
@@ -29,11 +33,30 @@ public class LanNetwork extends JDialog {
 	 * Create the dialog.
 	 */
 	public LanNetwork() {
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 325, 147);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(new BorderLayout(0, 0));
+		{
+			JLabel lblYourIpIs = new JLabel("Your IP is: ");
+			contentPanel.add(lblYourIpIs, BorderLayout.NORTH);
+			lblYourIpIs.setHorizontalAlignment(SwingConstants.CENTER);
+		}
+		{
+			JPanel panel = new JPanel();
+			contentPanel.add(panel, BorderLayout.CENTER);
+			panel.setLayout(new BorderLayout(0, 0));
+			{
+				JSeparator separator = new JSeparator();
+				panel.add(separator, BorderLayout.NORTH);
+			}
+			{
+				JLabel lblNewLabel = new JLabel("Waiting for establishing a connection...");
+				panel.add(lblNewLabel, BorderLayout.CENTER);
+				lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+			}
+		}
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
